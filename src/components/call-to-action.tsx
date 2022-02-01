0fa1d63db057224b70a378b8017a244f5be71396
Alexis <alexis_polak@hotmail.com>
2022-01-31 21:31:37 -0300

    Add the presskit and newsletter style

--- src/components/call-to-action.tsx
+++ src/components/call-to-action.tsx
@@ -4,23 +4,29 @@ import './call-to-action.css';
 const CallToAction = () => {
     return (
         <div className='section'>
-            <div className='newsletter'>
-                <p className='newsletter-message'>
-                    Want to know all the things? Announcements, behind the scenes, new games ?<br/>
-                    <strong>Subscribe to the newsletter.</strong> No spam, no passing your email address around. 
-                </p>
-                <form className='newsletter-subscribe'>
-                    <input type="text" name="name" />
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-            <div className='presskit'>
-                <p className='presskit-message'>
-                    Do you need information about the studio, logos and images of the games?<br/>
-                    <strong>Download the presskit.</strong>It has everything you need about the studio and the games. 
-                </p>
-            
-                <button className='presskit-download-button'> Download Seldoon presskit </button>
+            <h1> Keep in touch </h1>
+            <div className='section-container'>
+                <div className='newsletter'>
+                    <h2> Newsletter </h2>
+                    <p className='message'>
+                        Want to know all the things? Announcements, behind the scenes, new games ?<br/>
+                        <strong>Subscribe to the newsletter.</strong> No spam, no passing your email address around. 
+                    </p>
+                    <form className='newsletter-subscribe'>
+                        <input className='newsletter-input' type="text" name="name" />
+                        <input className='newsletter-button' type="submit" value="Submit" />
+                    </form>
+                </div>
+                <div className='presskit'>
+                    <h2> Presskit </h2>
+                    <p className='message'>
+                        Do you need information about the studio, logos and images of the games?<br/>
+                        <strong>Download the presskit.</strong>It has everything you need about the studio and the games. 
+                    </p>
+                    <div className='presskit-button'>
+                        <button className='presskit-download-button'> DOWNLOAD SELDOON PRESSKIT </button>
+                    </div>   
+                </div>
             </div>
         </div>
     );
