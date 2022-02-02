8aae3c976956bf13652cffdb664df50350fcff95
Alexis <alexis_polak@hotmail.com>
2022-02-02 19:09:38 -0300

    Added the indiana-drag-scroll package

--- src/components/gameplay.tsx
+++ src/components/gameplay.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './gameplay.css';
+import ScrollContainer from 'react-indiana-drag-scroll'
+
+import img1 from '../img/screenshots/img1.png';
 
 const Gameplay = () => {
+
 
     const automateCharacter = '';
     return (
@@ -11,9 +15,22 @@ const Gameplay = () => {
                     <source src="https://thumbs.gfycat.com/WillingSickHylaeosaurus-mobile.mp4" type="video/mp4"/>
                 </video>
             </div>
-            <div>
-                
-            </div>
+            <ScrollContainer className="scroll-container"
+                vertical={false}
+                horizontal={true}
+                hideScrollbars={true}
+                nativeMobileScroll={true}
+            >
+                <img src={img1} alt='menu screenshot' className='screenshot'/>
+                <img src={img1} alt='menu screenshot' className='screenshot'/>
+                <img src={img1} alt='menu screenshot' className='screenshot'/>
+                <img src={img1} alt='menu screenshot' className='screenshot'/>
+                <img src={img1} alt='menu screenshot' className='screenshot'/>
+                <img src={img1} alt='menu screenshot' className='screenshot'/>
+                <img src={img1} alt='menu screenshot' className='screenshot'/>
+                <img src={img1} alt='menu screenshot' className='screenshot'/>
+                <img src={img1} alt='menu screenshot' className='screenshot'/>
+            </ScrollContainer>
         </div>
     );
 }
