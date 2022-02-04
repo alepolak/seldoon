f9617ad1696b6c9eaae0a703b9a4adc80eb83037
Alexis <alexis_polak@hotmail.com>
2022-02-04 14:28:06 -0300

    Updated images sizes

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -9,19 +9,19 @@ import twitterIcon from '../img/twitter_icon.webp';
 const Social = () => {
     return (
         <div className='social'> 
-            <a href="mailto:seldoongames@gmail.com" target="_blank">
+            <a href="mailto:seldoongames@gmail.com" target="_blank" rel="noreferrer">
                 <img className="social-icon" src={mailIcon} alt='mail icon'/>
             </a>
-            <a href="https://twitter.com/SeldoonStudio" target="_blank">
+            <a href="https://twitter.com/SeldoonStudio" target="_blank" rel="noreferrer">
                 <img className="social-icon" src={twitterIcon} alt='twitter icon'/>
             </a>
-            <a href="https://www.instagram.com/seldoon/" target="_blank">
+            <a href="https://www.instagram.com/seldoon/" target="_blank" rel="noreferrer">
                 <img className="social-icon" src={instagramIcon} alt='instagram icon'/>
             </a>
-            <a href="https://www.youtube.com/channel/UC_8yUGTNcsr1F8j4_WIvrwg/videos" target="_blank">
+            <a href="https://www.youtube.com/channel/UC_8yUGTNcsr1F8j4_WIvrwg/videos" target="_blank" rel="noreferrer">
                 <img className="social-icon" src={youtubeIcon} alt='youtube icon'/>
             </a>
-            <a href="https://www.tiktok.com/@seldoongames" target="_blank">
+            <a href="https://www.tiktok.com/@seldoongames" target="_blank" rel="noreferrer">
                 <img className="social-icon" src={tiktokIcon} alt='tiktok icon'/>
             </a>
         </div>
